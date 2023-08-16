f84380fc92c4e1894dff3c87726dedc67758e84f
mhtanim <mhtanim2@gmail.com>
2023-08-17 01:29:28 +0600

    removing multer

--- src/api/users/manage-user/router.js
+++ src/api/users/manage-user/router.js
@@ -1,12 +1,11 @@
 
 const authenticator = require("../../../middleware/authenticator");
-const cloudinaryUploadMiddleware = require("../../../middleware/imageUplaod");
+//const cloudinaryUploadMiddleware = require("../../../middleware/imageUplaod");
 const { update_profile, search_by_email, search_by_cont_no, get_all_user, delete_all_user,get_users_info } = require("./controller");
-
 
 const manage_router = require("express").Router();
 
-manage_router.post("/user/profile/update/:email", cloudinaryUploadMiddleware, update_profile);
+manage_router.post("/user/profile/update/:email", /* cloudinaryUploadMiddleware, */ update_profile);
 manage_router.get("/user/search/email/:email", authenticator, search_by_email);
 manage_router.get("/user/list", get_all_user);
 manage_router.delete("/user/delete/all", authenticator, delete_all_user);
